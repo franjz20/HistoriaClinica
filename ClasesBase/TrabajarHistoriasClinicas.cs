1e78755bec863f1d73d60b6863334444705f7323
franco <francodanielj@gmail.com>
2026-01-06 20:10:19 -0300

    se separaron en dos listas dentro de TrabajarHistoriasClinicas

--- ClasesBase/TrabajarHistoriasClinicas.cs
+++ ClasesBase/TrabajarHistoriasClinicas.cs
@@ -16,7 +16,25 @@ namespace ClasesBase
                                     Integrated Security=True;
                                     Connect Timeout=30;";
 
+        public static DataTable listar_historias_clinicas()
+        {
+            SqlConnection cnn = new SqlConnection(connectionString);
 
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT hc.Fecha_Consulta, hc.Motivo_Consulta, hc.Diagnostico_Presuntivo, " +
+                              "p.Nombre + ' ' + p.Apellido as Nombre_Completo, p.DNI, DATEDIFF(YEAR, p.FechaNacimiento, GETDATE()) as Edad " +
+                              "FROM Historias_Clinicas hc " +
+                              "INNER JOIN Pacientes p ON hc.Paciente_Id = p.PacienteId " +
+                              "ORDER BY hc.Fecha_Consulta DESC";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
         public static DataTable listar_HC_porPaciente(int pacienteId)
         {
             SqlConnection cnn = new SqlConnection(connectionString);
@@ -32,6 +50,9 @@ namespace ClasesBase
                               "FROM Historias_Clinicas " +
                               "Where Paciente_Id = @pacienteId "+
                               "ORDER BY Fecha_Consulta DESC";
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
 
             cmd.Parameters.AddWithValue("@pacienteId", pacienteId);
 
